c385dce6e50dfb6efdea2a143d44468199bd2e96
Rcmcpe <rcmcpe@qq.com>
2020-08-27 02:06:57 +0800

    Changed TFM to adapt .NET 5.0 Preview 8

--- Crlib/Extension/XPath.cs
+++ Crlib/Extension/XPath.cs
@@ -1,20 +1,10 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 
 namespace REVUnit.Crlib.Extension
 {
     public static class XPath
     {
-        public static string? ExpandPath(string path)
-        {
-            if (string.IsNullOrWhiteSpace(path))
-                throw new ArgumentException("Value cannot be null or whitespace", nameof(path));
-            return Environment.GetEnvironmentVariable("path", EnvironmentVariableTarget.Machine)
-                ?.Split(';')
-                .Select(p => Path.Combine(p, path)).Where(File.Exists).FirstOrDefault();
-        }
-
         public static bool IsDirectory(string path)
         {
             if (string.IsNullOrWhiteSpace(path))
