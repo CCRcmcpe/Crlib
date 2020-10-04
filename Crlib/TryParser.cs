269538623ebd203e64904632376660c3a2079890
Rcmcpe <rcmcpe@qq.com>
2020-10-05 00:24:31 +0800

    New TryParser class, replacing the delegate with same name

--- Crlib/TryParser.cs
+++ Crlib/TryParser.cs
@@ -1,4 +1,31 @@
-﻿namespace REVUnit.Crlib
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace REVUnit.Crlib
 {
-    public delegate bool TryParser<in TSrc, T>(TSrc value, out T result);
+    public class TryParser<TSrc, T>
+    {
+        public delegate bool Agent(TSrc value, [MaybeNullWhen(false)] out T result);
+
+        private readonly Func<TSrc, T> _parser;
+
+        public TryParser(Func<TSrc, T> parser)
+        {
+            _parser = parser;
+        }
+
+        public bool TryParse(TSrc value, [MaybeNullWhen(false)] out T result)
+        {
+            try
+            {
+                result = _parser(value);
+                return true;
+            }
+            catch
+            {
+                result = default;
+                return false;
+            }
+        }
+    }
 }
